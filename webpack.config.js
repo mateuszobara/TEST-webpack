ff0643d9fb289e08800a5ca88b03b54f0ba223ff
Mateusz Obara <mateuszobara02@gmail.com>
2020-03-05 21:19:03 +0100

    Create basic webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'production',
@@ -6,5 +8,24 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js',
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'docs'),
+        port: '8080',
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/html/index.html'
+        }),
+        new MiniCssExtractPlugin({filename: 'style.css'}),
+    ]
 }
