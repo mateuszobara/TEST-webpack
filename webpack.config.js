4a810ae7309a3918f7d94afed7a577edb8dbebd6
Mateusz Obara <mateuszobara02@gmail.com>
2020-03-09 21:29:01 +0100

    Install babel and create basic setup

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,12 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { loader: "babel-loader" }
+            },
+
+            {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
